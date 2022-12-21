55150090f813f83cea04db83bb1a8b0c66819019
Ricardo Alexandre <rjfae@av.it.pt>
2022-12-21 18:23:56 +0000

    update shell script to install dependencies

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
+#!/usr/bin/env bash
+
+relativedirectory="$( dirname -- "$0"; )";
+cd "$relativedirectory";
+basedirectory="$( pwd; )";
+echo "Your base directory is #${basedirectory}#";
+
 sudo apt-get install python3-venv
 
-cd ~/sdrt/Working/Simulator/github/indoor-location-emulator/src/backend
+# src/backend module
+echo "open directory: src/backend";
+cd $basedirectory/src/backend
 python3 -m venv .venv
-source .venv/bin/activate
-pip3 install -r requirements.txt
+. "$basedirectory/src/backend/.venv/bin/activate"
+pip install -r requirements.txt
 
-cd ~/sdrt/Working/Simulator/github/indoor-location-emulator/src/ml-models
+# src/ml-models
+echo "open directory: src/ml-models";
+cd $basedirectory/src/ml-models
 python3 -m venv .venv
-source .venv/bin/activate
-pip3 install -r requirements.txt
+. "$basedirectory/src/ml-models/.venv/bin/activate"
+pip install -r requirements.txt
