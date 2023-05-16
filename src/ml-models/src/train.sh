14ecb2978e8e344de17e02c98f46cae95e6475ef
ricardoalexandre-dev <56154704+ricardoalexandre-dev@users.noreply.github.com>
2023-05-16 16:04:53 +0100

    Update train.sh

--- src/ml-models/src/train.sh
+++ src/ml-models/src/train.sh
@@ -1,7 +1,7 @@
 # Model Training:
 # The train.sh script was created as an easier and faster way to train all the models.
 # To use it, there must be a dataset which name starts with "rssi_antennas" in the same dir as the script, where shouldn't be stored any important model with the same name as the new ones, otherwise they will be overwritten.
-# The dir which the models will be moved into is the "somos_saude" by default, but it can be changed in the script.
+# The dir which the models will be moved into is the "library_ua" by default, but it can be changed in the script.
 
 python3 pos_train/knn_train.py
 python3 pos_train/random_forest_train.py
@@ -9,8 +9,8 @@ python3 pos_train/svr_train.py
 python3 pos_train/grad_boost_regr_train.py
 python3 pos_train/decision_tree_train.py
 
-# mv knn_model.sav somos_saude
-# mv random_forest_model.sav somos_saude
-# mv svr_model.sav somos_saude
-# mv grad_boost_regr_model.sav somos_saude
-# mv decision_tree_model.sav somos_saude
+# mv knn_model.sav library_ua
+# mv random_forest_model.sav library_ua
+# mv svr_model.sav library_ua
+# mv grad_boost_regr_model.sav library_ua
+# mv decision_tree_model.sav library_ua
